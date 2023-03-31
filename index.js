1b35c4d046732f5643f43b9f693dc1bde981d16c
meetmakwana19 <meetmakwana203@gmail.com>
2023-03-31 17:13:01 +0530

    Created base for product info page

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ a = document.body
 
 console.log(a)
 
-// -----------------
+// ----------------- GET all products part : 
 
 // XMLHttpRequest() is a constructor directly and not a class in JS
 // The constructor initializes an XMLHttpRequest. It must be called before any other method calls.
@@ -16,14 +16,17 @@ const httpRequest = new XMLHttpRequest()
 httpRequest.onreadystatechange = function(){
     if(httpRequest.readyState === XMLHttpRequest.DONE){
         if(httpRequest.status === 200){
-            console.log("API Response: ", JSON.parse(httpRequest.responseText));
+            // console.log("API Response: ", JSON.parse(httpRequest.responseText));
 
             // parsing the responseText which is in string means string to JSON
             const productsArray = JSON.parse(httpRequest.responseText)
 
             productsArray.map(
                 function(prod) {
-                    productsDiv.appendChild(createProducts(prod))
+                    // if condition because using the same JS file for other html page too which doesnt need this function
+                    if(productsDiv){
+                        productsDiv.appendChild(  createProducts(prod))
+                    }
                 }
             )
         }
@@ -33,12 +36,11 @@ httpRequest.onreadystatechange = function(){
     }
 }
 
-httpRequest.open("GET", "https://fakestoreapi.com/products")
 
+httpRequest.open("GET", "https://fakestoreapi.com/products")
 httpRequest.send()
 
-// products
-
+// ---------------- Populating all products on index.html
 const productsDiv = document.querySelector("#products-grid")
 
 function createProducts(prod){
@@ -61,13 +63,14 @@ function createProducts(prod){
 }
 
 // -------- Nav items
+// -------- GET all categories :
 
 const categories_req = new XMLHttpRequest()
 
 categories_req.onreadystatechange = function(){
     if(categories_req.readyState === XMLHttpRequest.DONE){
         if(categories_req.status === 200){
-            console.log("API Response: ", JSON.parse(categories_req.responseText));
+            // console.log("API Response: ", JSON.parse(categories_req.responseText));
 
             const categoriesArray = JSON.parse(categories_req.responseText)
             console.log("categoriesArray : ",categoriesArray);
@@ -86,7 +89,6 @@ categories_req.onreadystatechange = function(){
 
 
 const navCollapseDiv = document.querySelector(".collapse")
-// console.log(navCollapseDiv);
 
 function createNavItems(cats){
     const navUlEl = document.createElement("ul")
@@ -115,3 +117,52 @@ navForm.innerHTML = `
 <button class="btn btn-outline-success" type="submit">Search</button>
 `
 navCollapseDiv.appendChild(navForm)
+
+// ---------------Product page part for product.html:
+
+const productInfo_req = new XMLHttpRequest();
+
+productInfo_req.onreadystatechange = function(){
+    if(productInfo_req.readyState === XMLHttpRequest.DONE){
+        if(productInfo_req.status === 200){
+            console.log("API Respone :", JSON.parse(productInfo_req.responseText));
+
+            const infoArray = JSON.parse(productInfo_req.responseText)
+
+            // infoArray.map(function(prod){
+            //     prodContainer.appendChild(createInfo(prod))
+            // })
+            prodContainer.appendChild(createInfo(infoArray))
+        }
+        else{
+            alert("Some error occured.")
+        }
+    }
+}
+productInfo_req.open("GET", "https://fakestoreapi.com/products/1")
+productInfo_req.send();
+
+// ---------------- Populating product's info on product.html
+const prodContainer = document.querySelector("#prod-container")
+
+function createInfo(prod){
+    console.log("creting from", prod);
+
+    const infoPage = document.createElement("section")
+    infoPage.classList.add("row")
+
+    infoPage.innerHTML = `
+    <p>> ${prod.category.charAt(0).toUpperCase() + prod.category.slice(1)}</p>
+    <div class="col-lg-6 text-center border p-5">
+        <img src="${prod.image}" width="auto" class="">
+    </div>
+    <div class="col-lg-6 border p-5">
+        <h3>${prod.title}</h3>
+        <p>${prod.rating}</p>
+        <h6>${prod.price}</h6>
+        <p>${prod.description}</p>
+    </div>
+    `
+
+    return infoPage;
+}
